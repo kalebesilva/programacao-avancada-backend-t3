3730287a08e5ae3a80650c889ac3b9baf2b44a83
vrcvieira <victor.vieira@ifac.edu.br>
2023-06-26 20:06:21 -0500

    update jun26

--- first-project-node/script.js
+++ first-project-node/script.js
@@ -33,6 +33,8 @@ Agora, utilize o método setInterval apenas uma vez no lugar de setTimeout e res
 
 */
 
+/*
+
 const fs = require('fs')
 console.log('Primeira saída.')
 fs.readFile('./text.txt',callback)
@@ -42,3 +44,76 @@ function callback(err, conteudo){
 }
 console.log('Segunda saída.')
 console.log('Terceira saída.')
+
+*/
+/*
+
+function soma(){
+    const resultado = 2 + 1
+    if(resultado === 2) sucesso()
+    else erro()
+}
+function sucesso(){
+    console.log('Sucesso! A soma é 2.')
+}
+function erro(){
+    console.log('Erro! A soma não deu 2.')
+}
+soma()
+
+*/
+// o código acima é síncrono, ou seja, não estamos passando funções como argumentos
+// vamos transformar num código assíncrono (com callback)
+
+function soma(callback, callbackErro){
+    const resultado = 3 + 1
+    if (resultado === 2) callback()
+    else callbackErro()
+}
+soma( () => { console.log('Sucesso! A soma é 2.') } , () => { console.log('Erro! A soma não deu 2.') } )
+
+
+// Promises
+// São classes que possuem métodos específicos para quando o resultado de uma execução for concluído com sucesso ou quando fugir do esperado.
+// No construtor, é passada como argumento uma função anônima que possui dois parâmetros:
+// resolve - o que vai ser executado quando o resultado for o esperado;
+// reject - o que vai ser executado quando o resultado não for o esperado.
+// resolve e reject são métodos que, quando executados, também passam argumentos que são resolvidos no then e no catch.
+
+const p = new Promise((resolve, reject) => {
+    const resultado = 1 + 1
+    if (resultado === 2) resolve('Sucesso! A soma é 2.')
+    else reject('Erro! A soma não deu 2.')
+})
+
+p.then((mensagem) => {
+    console.log(mensagem) 
+}).catch((mensagem) => {
+    console.log(mensagem)
+})
+
+
+// voltando para callback
+
+const melhorProgramador = 'João Felix'
+
+function oMelhor(callback, callbackErro){
+    if(melhorProgramador == 'João Felix'){
+        callback({
+            nome: melhorProgramador,
+            mensagem: 'humildemente o melhor!'
+        })
+    }else{
+        callbackErro({
+            mensagem01: 'Tá errado...',
+            mensagem02: melhorProgramador + ', sério? '
+        })
+    }
+}
+oMelhor((objeto)=>{
+    console.log(objeto.nome + ' é ' + objeto.mensagem)
+}, (objeto)=>{
+    console.log(objeto.mensagem02 + objeto.mensagem01)
+})
+
+// Transforme essa callback em Promise.
