38682c56b692e0123f6064e6a20ff436c335e3fe
kalebesilva <kalebe.silvadev@gmail.com>
2023-06-26 19:09:28 -0500

    Update sumCallBack.js

--- students/kalebe/9 -  convert-To-Callback/sumCallBack.js
+++ students/kalebe/9 -  convert-To-Callback/sumCallBack.js
@@ -1,13 +1,10 @@
-
 function soma(resultado) {
-    if(resultado() == true) console.log("Sucesso")
-    else console.log("Erro")
-
-    
+  if (resultado() == true) console.log("Sucesso");
+  else console.log("Erro");
 }
 
-soma( (()=>{
-    const resultado = 2;
-    if(resultado === 2 )return true
-    else return false
-}) );
+soma(() => {
+  const resultado = 2;
+  if (resultado === 2) return true;
+  else return false;
+});
