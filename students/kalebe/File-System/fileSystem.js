4862cae75c5840c86bcaf6d9bc2515ddfa9522a0
kalebesilva <kalebe.silvadev@gmail.com>
2023-06-26 19:08:37 -0500

    comentário de file system

--- students/kalebe/File-System/fileSystem.js
+++ students/kalebe/File-System/fileSystem.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 
 console.log("Primeira saída");
-fs.readFile("./file.txt",(err, dataOfFile)=> {
-    if(err) return console.error(err);
+
+fs.readFile("./file.txt",(err, dataOfFile)=> { // Basicamente esse método read File só funciona 
+    if(err) return console.error(err);// de maneira assincrona, entao, passamos uma arrow function para utilizar callback
     console.log(String(dataOfFile));
 
 });
