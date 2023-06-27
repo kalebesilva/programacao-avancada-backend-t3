7ff356b7c7c0474eff8de16947ca2297fb12d38f
kalebesilva <kalebe.silvadev@gmail.com>
2023-06-26 19:08:43 -0500

    Create sumCallBack.js

---
+++ students/kalebe/9 -  convert-To-Callback/sumCallBack.js
@@ -0,0 +1,13 @@
+
+function soma(resultado) {
+    if(resultado() == true) console.log("Sucesso")
+    else console.log("Erro")
+
+    
+}
+
+soma( (()=>{
+    const resultado = 2;
+    if(resultado === 2 )return true
+    else return false
+}) );
