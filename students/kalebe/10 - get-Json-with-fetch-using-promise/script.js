4e52e9ca63c27bcc87d111e875aa0e0d964ccf18
Kalebe <kalebe.silvadev@gmail.com>
2023-06-28 00:09:25 -0300

    finalizando a atividade

--- students/kalebe/10 - get-Json-with-fetch-using-promise/script.js
+++ students/kalebe/10 - get-Json-with-fetch-using-promise/script.js
@@ -2,9 +2,24 @@ let myList = document.querySelector(".myList");
 
 let myObjVector = new Array();
 
-
-fetch("https://jsonplaceholder.typicode.com/users")
+getData();
+async function getData() {
+  await fetch("https://jsonplaceholder.typicode.com/users")
     .then((theResponse) => theResponse.json())
-    .then((data) => JSON.parse(data)).then((data)=> console.log(data.name))
+    .then((data) =>
+      data.map((x) => myObjVector.push({ name: x.name, email: x.email }))
+    )
+    .catch((err) => console.error(err));
 
+  myObjVector.forEach((element) => {
+    let liNome = document.createElement("li");
+    let liEmail = document.createElement("li");
+    liNome.textContent = element.name;
+    liEmail.textContent = element.email;
+    myList.appendChild(liNome);
+    myList.appendChild(liEmail);
+  });
+  document.body.appendChild(myList);
+}
 
+console.log(myObjVector);
