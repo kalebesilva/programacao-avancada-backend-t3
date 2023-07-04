df0eb7b476a5fc2c8521a35e4b14ee0762082d63
kalebesilva <kalebe.silvadev@gmail.com>
2023-07-04 18:44:03 -0500

    atualizacao

---
+++ students/kalebe/project-favorites/server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const path = require("path");
+
+http.createServer((req, res)=>{
+  
+    if(req.url === '/'){
+     fs.readFile(path.join(__dirname, "public", "index.html"),(err, content)=>{
+            if(err) throw err;
+            res.end(content);
+        });
+    }
+    if(req.url === '/contato'){
+        return res.end('contato');
+
+    }
+   
+}).listen(5000, ()=> {
+    console.log("Running");
+    
+});
