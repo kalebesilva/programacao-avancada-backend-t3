8d9f40b7fc422352adcaf4e3415f2ad2fd9d9791
vrcvieira <victor.vieira@ifac.edu.br>
2023-07-07 20:12:38 -0500

    update 07jul

--- project-favorites/public/scripts.js
+++ project-favorites/public/scripts.js
@@ -23,7 +23,7 @@ function addElement({ name, url }) {
     a.target = "_blank"
 
     trash.innerHTML = "x"
-    trash.onclick = () => removeElement(trash)
+    trash.onclick = () => removeElement(trash, { name, url })
     
     ul.append(li)
     li.append(a)
@@ -42,9 +42,16 @@ async function addElementAndSendToApi({ name, url }){
 
 }
 
-function removeElement(element) {
-    if (confirm('Tem certeza que deseja deletar?'))
+async function removeElement(element, { name, url }) {
+    if (confirm('Tem certeza que deseja deletar?')){
         element.parentNode.remove()
+
+        const response = await fetch(`http://localhost:3000/?name=${name}&url=${url}&del=1`)
+
+        if (!response.ok)
+            console.error(`Erro ao enviar os dados para a API: ${response.statusText}`)
+    }
+
 }
 
 form.addEventListener('submit', (event) => {
